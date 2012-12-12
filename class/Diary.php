aeb26128659c1314a88da733b98668b9cc73a794
wangdahu <553784858@qq.com>
2012-12-12 17:46:05 +0800

    test

---
+++ class/Diary.php
@@ -0,0 +1,9 @@
+<?php
+
+class Diary{
+
+    public static function test(){
+        return 1;
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include_once('class/Diary.php');
+$diary = new Diary();
+echo Diary::test();
+
