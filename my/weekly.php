786d959dacf82cf7009b629823674541762594d5
wangdahu <553784858@qq.com>
2012-12-21 17:58:22 +0800

    周日期算法有误

--- my/weekly.php
+++ my/weekly.php
@@ -1,17 +1,17 @@
 <?php
 $title = "周报";
 $type = 'weekly';
-
+$mondayTime = date('w') == 1 ? strtotime("this Monday") : strtotime("-1 Monday");
 // 向前向后翻天
 $forward = isset($_GET['forward']) ? $_GET['forward'] : 0;
 if($forward){
     $forwardWeeks = $forward + 1;
     $backwardWeeks = $forward - 1;
-    $startTime = strtotime("this Monday") - $forward*86400*7;
+    $startTime = $mondayTime - $forward*86400*7;
 }else{
     $forwardWeeks = 1;
     $backwardWeeks = -1;
-    $startTime = strtotime("this Monday");
+    $startTime = $mondayTime;
 }
 $endTime = $startTime + 7*86400 - 1;
 $weekDate = array();
