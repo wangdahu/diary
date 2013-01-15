3e2d99e23be5cf61168649c5e456ed6a10ea5efc
wangdahu <553784858@qq.com>
2013-01-15 18:12:15 +0800

    标签页面处理

--- class/DiaryDaily.php
+++ class/DiaryDaily.php
@@ -42,7 +42,7 @@ class DiaryDaily{
         $result = $diary->db->query($sql);
         $list = array();
         while($row = $result->fetch_array(MYSQLI_ASSOC)) {
-            $list[] = $row;
+            $list[$row['id']] = $row;
         }
         return $list;
     }
@@ -65,6 +65,19 @@ class DiaryDaily{
     }
 
     /**
+     * 获取当前tag的日报信息
+     */
+    public static function getTagDailys($diary, $tag_id) {
+        $sql = "select * from `diary_info` where `id` in (select `diary_id` from `diary_daily_tag` where `tag_id` = $tag_id)";
+        $result = $diary->db->query($sql);
+        $dailys = array();
+        while($daily = $result->fetch_array(MYSQLI_ASSOC)) {
+            $dailys[$daily['id']] = $daily;
+        }
+        return $dailys;
+    }
+
+    /**
      * 获取日报的tag名列表
      */
     public static function getDailyTagName($diary, $daily_id) {
@@ -81,9 +94,6 @@ class DiaryDaily{
         return $tagList;
     }
 
-    /**
-     * 获取有内容但未汇报的日报
-     */
     public static function noReportDaily($diary, $firstTime, $lastTime, $type, $uid = null) {
         $uid = $uid ? $uid :$diary->uid;
         $uninStr = '%Y-%m-%d';
--- my/tagDaily.php
+++ my/tagDaily.php
@@ -1,107 +1,28 @@
 <?php
-$title = "我的日志";
-$type = 'daily';
-$weekarray = array("日","一","二","三","四","五","六");
+$tagId = (int)$_GET['tag'];
+$title = "我自己的标签";
+// 获取当前tag的所有日志
+$dailys = DiaryDaily::getTagDailys($diary, $tagId);
 
-$uid = $diary->uid;
-$corpId = $diary->corpId;
-
-// 向前向后翻天
-$forward = isset($_GET['forward']) ? (int)$_GET['forward'] : 0;
-if($forward){
-    $forwardDays = $forward + 1;
-    $backwardDays = $forward - 1;
-    $object = date('Y-m-d',time() - $forward*86400);
-}else{
-    $forwardDays = 1;
-    $backwardDays = -1;
-    $object = date('Y-m-d',time());
-}
-$startTime = strtotime($object);
-$endTime = $startTime + 86400 - 1;
-
-// 该企业该用户在选择时间内的日报
-$rowsSql = "select * from `diary_info` where `uid` = $uid and `corp_id` = $corpId and `type` = 1 and `show_time` between $startTime and $endTime order by id desc";
-$result = $diary->db->query($rowsSql);
-$dailys = array();
-while($row = $result->fetch_array(MYSQLI_ASSOC)){
-    $dailys[] = $row;
-};
 $num = count($dailys);
-
 $userTags = DiaryDaily::getUserTags($diary);
-// 获取颜色列表
-$colorList = DiaryDaily::getColorList($diary);
-$tagList = DiaryDaily::getTagList($diary);
-
-$defaultColorId = rand(1,20);
-$showCommit = false;
-// 判断是否为补交/未汇报/已汇报
-if($forward < 0) { // 未来
-    $isReported = $allowPay = false;
-}else if($forward == 0) { // 今天
-    // 是否已过汇报时间
-    $reportTime = DiarySet::reportTime($diary);
-    $dailyTime = $reportTime['dailyReport']['hour'].":".$reportTime['dailyReport']['minute'];
-    $isReported = $allowPay = false;
-    if(time() > strtotime($object." ".$dailyTime)){ // 已过汇报时间
-        $isReported = DiaryReport::checkReport($diary, $type, $object);
-        $allowPay  = $isReported ? false : true;
-        $showCommit = $isReported;
-    }
-}else{ // 过去
-    $isReported = DiaryReport::checkReport($diary, $type, $object);
-    $allowPay = $isReported ? false : true;
-    $showCommit = true;
-}
+$tagName = $userTags[$tagId]['tag'];
 ?>
 
 <?php include "views/layouts/header.php"; ?>
-<?php include "views/my/top.php"; ?>
+<?php include "views/my/tag-top.php"; ?>
 
 <div class="content">
     <!--今日工作开始-->
     <div class="content_bar mb25">
-        <h2 class="content_tit clearfix">
-            <p>今日工作：<em><?php echo $num;?> 项</em></p>
-            <?php if($allowPay):?>
-            <?php if($num):?>
-            <a href="javascript:" class="fr mr10 pay-diary js-pay_diary"></a>
-            <?php else:?>
-            <a class="fr mr10 pay-disabled"></a>
-            <?php endif;?>
-            <?php endif;?>
-            <?php if(!$isReported):?>
-            <a href="javascript:" class="write-<?php echo $type?> fr mr10"></a>
-            <?php endif;?>
-        </h2>
-        <?php if(!$num):?>
-        <div class="c_t mt10"></div>
-        <div class="c_c">
-            <div class="c_c_c">
-                <div>
-                    <p style="font-size: 16px;color: red; text-align: center; line-height: 100px;">
-                        <strong>还未填写任何日志内容</strong>
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="c_b"></div>
-        <?php else:?>
+        <h2 class="content_tit clearfix" style="border: none;"></h2>
         <?php foreach($dailys as $daily):?>
         <div class="c_t mt10"></div>
         <div class="c_c">
             <div class="c_c_c">
-                <?php if($isReported):?>
                 <div>
                     <p><?php echo nl2br($daily['content']); ?></p>
                 </div>
-                <?php else:?>
-                <div data-daily_id="<?php echo $daily['id']; ?>" class="js-edit_diary" style="cursor: pointer">
-                    <p><?php echo nl2br($daily['content']); ?></p>
-                    <div style="display:none;"><?php echo $daily['content'];?></div>
-                </div>
-                <?php endif;?>
                 <div class="clearfix diary-operation" >
     <?php
         $tagList = array();
@@ -109,7 +30,6 @@ if($forward < 0) { // 未来
         $tagIds = array_keys($tagList);
     ?>
                     <span class="daily-date"><?php echo date('y-m-d H:i', $daily['fill_time']);?></span>
-                    <?php if(!$isReported):?>
                     <a href="javascript:;" data-diary_id="<?php echo $daily['id'];?>" class="delete js-del_daily"></a>
                     <div class="add-tag-wrapper">
                         <a href="javascript:;"  class="add_tag js-opterate_tag"></a>
@@ -129,20 +49,20 @@ if($forward < 0) { // 未来
                             <?php include "tag.php"; ?>
                         </div>
                     </div>
-                    <?php endif;?>
                     <span class="tag-list" id="tag-list-<?php echo $daily['id'];?>">
                         <?php foreach($userTags as $tag):?>
                         <div class="js-tag" id="diary_tag_<?php echo $tag['id'];?>" data-tag_id="<?php echo $tag['id'];?>" data-diary_id="<?php echo $daily['id'];?>" style="float: left; margin: 0 4px; background-color: <?php echo $tag['color']?>; <?php echo in_array($tag['id'], $tagIds) ? '' : 'display: none;'?>">
                             <div title="<?php echo $tag['tag'];?>" id="tag-<?php echo $tag['id'];?>" class="ellipsis" style="max-width: 120px; float: left; ">
+                                <?php $url = "/diary/index.php/my/tagDaily?tag=".$tag['id']; ?>
+                                <a style="text-decoration: none;" href="<?php echo $url;?>">
                                 <span style="margin:4px;">
                                     <?php echo $tag['tag'];?>
                                 </span>
+                                </a>
                             </div>
-                            <?php if(!$isReported):?>
                             <div class="js-del_tag" style="float: left; display:none;">
                                 <a href="javascript:;" class="dtag"></a>
                             </div>
-                            <?php endif;?>
                         </div>
                         <?php endforeach;?>
                     </span>
@@ -151,17 +71,11 @@ if($forward < 0) { // 未来
         </div>
         <div class="c_b"></div>
         <?php endforeach;?>
-        <?php endif;?>
     </div>
-    <!--今日工作结束-->
-    <?php if($showCommit):
-          include dirname(dirname(__FILE__))."/team/comment.php";
-    endif;?>
 </div>
 
 <?php include "views/layouts/footer.php"; ?>
 <?php include "views/set/addTag.php"; ?>
-<?php include "views/my/createDaily.php"; ?>
 
 <script>
     $(function() {
