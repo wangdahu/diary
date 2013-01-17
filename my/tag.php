52774ad375cdf88e24c5c18973cf6edd488a4e5f
wangdahu <553784858@qq.com>
2013-01-17 18:23:11 +0800

    删除标签文字修改

--- my/tag.php
+++ my/tag.php
@@ -1,7 +1,7 @@
 <div style="line-height: 30px;">
     <div style="border-top: 1px solid #ccc"></div>
     <div style="margin-left: 30px;">
-        <a href="javascript:;" data-diary_id="<?php echo $daily['id'];?>" class="js-del-all">删除所有标签</a>
+        <a href="javascript:;" data-diary_id="<?php echo $daily['id'];?>" class="js-del-all">取消所有标签</a>
     </div>
     <div style="border-top: 1px solid #ccc"></div>
     <div style="margin-left: 30px;">
