5cb755594cab2451876e68ae89a8fcd3fc8067ff
wangdahu <553784858@qq.com>
2013-01-20 19:37:54 +0800

    style

--- views/team/tag-top.php
+++ views/team/tag-top.php
@@ -4,7 +4,7 @@ $wiseucUrl = "wisetong://message/?uid=".$userInfo['LoginName']."&myid=".$diary->
 ?>
 <!--功能操作开始-->
 <div class="todo clearfix">
-    <a href="javascript:history.back(-1);" class="a_01 fl">返回</a>
+    <a href="javascript:" onclick="history.back(-1);" class="a_01 fl">返回</a>
      <p class="fl mg10" ><strong><span style="font-size: 16px">【<?php echo $tagName?>】</span>  <a href="<?php echo $wiseucUrl;?>"><?php echo $userInfo['UserName'];?></a><?php echo "（".$userInfo['dept_name']."）";?> 共有 <?php echo $num;?> 项工作</strong></p>
 </div>
 <!--功能操作结束-->
