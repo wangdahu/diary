6a5c510a95fa95fe2f13a1b359416f3d5e2aee4e
Brook <uedsky@gmail.com>
2013-01-27 12:20:38 +0800

    add an option for google closure compiler

--- source/jqueryUI/js/compile.sh
+++ source/jqueryUI/js/compile.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # get optional optimization instruction
-
 opt=''
 if [ "${1:0:2}" = "--" ]; then
     opt=${1:2}
@@ -19,7 +18,18 @@ cat $@ > "$out"
 if [ "$opt" = NONE ]; then
     echo "No min"
 else
-    curl --data-urlencode "text@$out" uedsky.com/tool/min  -o "$out"
+    curl \
+        -o "$out" \
+        --data-urlencode "text@$out" \
+        http://uedsky.com/tool/min
+    # curl \
+    #     -o "$out" \
+    #     --data-urlencode "js_code@$out" \
+    #     -d compilation_level=$opt \
+    #     -d output_info=compiled_code \
+    #     -d output_format=text \
+    #     -d 'output_wrapper=(function(){%25output%25}).call(this);' \
+    #     http://closure-compiler.uedsky.com/compile
 fi
 
 echo "created $out"
