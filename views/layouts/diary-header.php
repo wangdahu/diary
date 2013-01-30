2e6eb009ff52c7782fd46ef240a51a378a2fca31
wangdahu <553784858@qq.com>
2013-01-30 09:33:29 +0800

    提示样式

--- views/layouts/diary-header.php
+++ views/layouts/diary-header.php
@@ -1,3 +1,3 @@
-<?php if($diary->isSetReport):?>
-<div style="background: #fff; margin-bottom: 20px; height: 30px; line-height: 30px; text-align: center; font-size: 14px;">现在还没有设置，赶快去<a style="font-size: 16px;" href="/diary/index.php/set/index">设置吧</a></div>
+<?php if(!$diary->isSetReport):?>
+<div style="background: #fff2c2; margin-top: -18px; margin-bottom: 20px; height: 30px; line-height: 30px; text-align: center; font-size: 14px; font-family: 宋体">尽快完善您的日志设置，一切将更轻松！<a style="font-size: 16px; padding-left: 20px;" href="/diary/index.php/set/index"><strong>现在就去</strong></a></div>
 <?php endif;?>
