57e8f41304012876c0edaaf5f83909242b1d48c3
wangdahu <553784858@qq.com>
2013-04-11 20:26:04 +0800

    float

--- views/layouts/diary-header.php
+++ views/layouts/diary-header.php
@@ -1,6 +1,6 @@
-<?php if(!(isset($_COOKIE['setReportRemind']) && $_COOKIE['setReportRemind'] == 'no') && !$diary->isSetReport):?>
-<div id="showRemindReport" style="background: #fff2c2; margin-top: -18px; margin-bottom: 20px; height: 30px; line-height: 30px; text-align: center; font-size: 14px; font-family: 宋体">尽快完善您的日志设置，一切将更轻松！<a style="font-size: 16px; padding-left: 20px;" href="/diary/index.php/set/index"><strong>现在就去</strong></a><a id="no-remind" style="margin-right: 10px; float: right;" href="javascript:"><strong>不再提醒</strong></a></div>
-<?php endif;?>
+<?php if(!(isset($_COOKIE['setReportRemind']) && $_COOKIE['setReportRemind'] == 'no') && !$diary->isSetReport): ?>
+<div id="showRemindReport" style="background: #fff2c2; margin-top: -18px; margin-bottom: 20px; height: 30px; line-height: 30px; text-align: center; font-size: 14px; font-family: 宋体"><a id="no-remind" style="margin-right: 10px; float: right;" href="javascript:"><strong>不再提醒</strong></a>尽快完善您的日志设置，一切将更轻松！<a style="font-size: 16px; padding-left: 20px;" href="/diary/index.php/set/index"><strong>现在就去</strong></a></div>
+<?php endif; ?>
 <script>
     $('#no-remind').click(function() {
         var exp = new Date(),
